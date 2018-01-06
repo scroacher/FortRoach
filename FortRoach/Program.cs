47e64620d42d41c361a849e8b09eceddefc51848
scroacher <s.roach@yahoo.com>
2018-01-05 20:51:03 -0500

    upd again

--- FortRoach/Program.cs
+++ FortRoach/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -12,13 +13,38 @@ namespace FortRoach
 {
     public class Program
     {
-        void Main(string[] args)
+        public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-				IWebHost BuildWebHost(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
-               .UseStartup<Startup>()
-               .Build();
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+            // 2nd line added
+
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+            // 3rd line added
+
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .UseUrls(url) // 4th line added
+            .Build();
+
+            host.Run();
+        }
+    }
+}
+    //public class Program
+    // {
+    //     public static void Main(string[] args)
+    //     {
+    //         BuildWebHost(args).Run();
+    //     }
+
+    //     public IWebHost BuildWebHost(string[] args) =>
+    //         WebHost.CreateDefaultBuilder(args)
+    //             .UseStartup<Startup>()
+    //             .Build();
+    // }
 			
 			
 			
@@ -36,8 +62,3 @@ namespace FortRoach
             // .Build();
 
             // host.Run();
-        }
-
-        
-    }
-}
